306b336a2831d73b7baf423740a039c92160337b
ofarook060 <ofarook060@gmail.com>
2026-07-19 08:55:17 +0000

    1911

--- api/renew-session.php
+++ api/renew-session.php
@@ -1,16 +1,16 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit(0);
 }
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../classes/connect.php';
-require_once __DIR__ . '/../../classes/jwt.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../classes/connect.php';
+require_once __DIR__ . '/../classes/jwt.php';
 
 $token = JWTAuth::extract_token();
 if (!$token) {
